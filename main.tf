5f3f01786b9f353fba95d779a86b7cef5bcbaeed
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-09 22:25:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 locals {
 
  test = 1
+ 
 
+}
+
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  provisioner "local-exec" {
+    command = "echo abcd"
+  }
 }
 
