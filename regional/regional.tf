f7d4acef93dba962d71eff456d49501c3a257a37
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-10 21:09:50 +0000

    Update regional.tf

--- regional/regional.tf
+++ regional/regional.tf
@@ -2,14 +2,14 @@ locals {
   abcd = ""
 }
 
-resource "null_resource" "regional1" {
+resource "null_resource" "regional4" {
   # Changes to any instance of the cluster requires re-provisioning
   provisioner "local-exec" {
     command = "echo abcd"
   }
 }
 
-resource "null_resource" "regional2" {
+resource "null_resource" "regional3" {
   # Changes to any instance of the cluster requires re-provisioning
   provisioner "local-exec" {
     command = "echo abcd"
