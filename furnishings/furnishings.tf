6db0b62d0777488f46068823b331600ca81a2d0c
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-18 01:58:03 +0000

    Update and rename furniture/furniture.tf to furnishings/furnishings.tf

--- furnishings/furnishings.tf
+++ furnishings/furnishings.tf
@@ -3,14 +3,14 @@ locals {
   abcd = ""
 }
 
-resource "null_resource" "furniture1" {
+resource "null_resource" "furnishings1" {
   # Changes to any instance of the cluster requires re-provisioning
   provisioner "local-exec" {
     command = "echo abcd"
   }
 }
 
-resource "null_resource" "furniture2" {
+resource "null_resource" "furnishings2" {
   # Changes to any instance of the cluster requires re-provisioning
   provisioner "local-exec" {
     command = "echo abcd"
